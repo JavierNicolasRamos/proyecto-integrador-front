c93e6ce04d44a2084a04919074f3d7df001d6e8b
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-10-18 19:39:35 -0300

    basic footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
-
+import React from "react";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-logo">
+        <p>©2023 Digital Booking</p>
+      </div>
+      <div className="footer-icons">
+        <img src="/src/Images/ico-facebook.png" alt="ico-facebook" />
+        <img src="/src/Images/ico-instagram.png" alt="ico-instagram" />
+        <img src="/src/Images/ico-tiktok.png" alt="ico-tiktok" />
+        <img src="/src/Images/ico-whatsapp.png" alt="ico-whatsapp" />
+      </div>
+    </footer>
+  );
 }
 
 export default Footer
