4310c4054e50fd1797c10cc2de99ba92552fc527
Federico Galan <fegv.2@hotmail.com.ar>
2023-10-18 01:23:27 -0300

    fix: minor changes on the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
