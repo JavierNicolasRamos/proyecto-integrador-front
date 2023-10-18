33f88a90dd59d62a06f0d44d152c7338b863bbe7
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-10-17 22:09:57 -0300

    initial routing ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-
-    </>
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
   )
 }
 
