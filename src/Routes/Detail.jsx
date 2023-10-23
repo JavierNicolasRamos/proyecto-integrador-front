b0ee6b7c63d41264dae2907ab99fd15e52441d5a
Federico Galan <fegv.2@hotmail.com.ar>
2023-10-23 10:19:21 -0300

    Refactor: The strucutre and imports was modified and hooks was added

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,29 +1,46 @@
 import { useParams } from 'react-router-dom';
 import productos from '../components/Product';
-import "./Detail.css";
+import "../styles/Detail.css";
+import { useState, useEffect } from 'react';
 
 const Detail = () => {
-  const { id } = useParams();
-  const producto = productos.find((p) => p.id === Number(id));
 
-  if (!producto) {
-    return <div>Producto no encontrado</div>;
-  }
+  const [productExists, setProductExists] = useState(false);
+  const [producto, setProducto] = useState({});
+  
+  // Parameter URL
+  const { id } = useParams();
+
+  useEffect(() => {
+    // Realiza la búsqueda del producto cuando el componente se monta
+    const foundProduct = productos.find(producto => producto.id == id);
+    if (foundProduct) {
+      setProducto(foundProduct);
+      setProductExists(true);
+    }
+  }, [id]);
+
+  const { nombre, imagen, precio, descripcion } = producto;
 
   return (
     <div className="detail-container">
-      <h2 className="detail-title">Detalle del Producto</h2>
-      <div className="product-details">
-        <img src={producto.imagen} alt={producto.nombre} className="product-image" />
-        <div className="product-info">
-          <h3 className="product-name">{producto.nombre}</h3>
-          <p className="product-description">{producto.descripcion}</p>
-          <p className="product-price">Precio: ${producto.precio.toFixed(2)}</p>
-        </div>
-      </div>
+      {productExists === false ? (
+        <div>Producto no encontrado</div>
+      ) : (
+        <>
+          <h2 className="detail-title">Detalle del Producto</h2>
+          <div className="product-details">
+            <img src={imagen} alt={nombre} className="product-image" />
+            <div className="product-info">
+              <h3 className="product-name">{nombre}</h3>
+              <p className="product-description">{descripcion}</p>
+              <p className="product-price">Precio: ${precio}</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
-};
-
+}
 
 export default Detail;
