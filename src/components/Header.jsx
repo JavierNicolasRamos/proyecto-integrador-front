b0710856620c23327e938df333d7e311fb06bb12
Federico Galan <fegv.2@hotmail.com.ar>
2023-10-24 17:06:39 -0300

    feat: Navigation menu added to header and its condicional css to show the searchbar if the path is home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Header.css";
 import { SearchIcon } from "./SearchIcon";
+import { Menu } from "./Menu";
 
 export const Header = () => {
+  const location = useLocation();
+
+  // Verifica si la ruta actual es la página de inicio
+  const isHome = location.pathname === "/home"
+
+
   return (
     <>
       <section className="phone">
         <p className="phone-text">🔥 3, 6, 12 y 18 Cuotas fijas EN TODOS LOS PRODUCTOS - 📞 11 3984 9613</p>
       </section>
-      <header className="header">
+      <header className={`header ${!isHome ? 'hide' : ''}`}>
         <section className="header__search-bar">
-          <Link to={"/home"}>
+          <Link to={"/home"} className="header__search-bar__logo">
             <img className="header__search-bar__img" src="src/images/header-logo.svg" alt="Imagen del logo de music rental" />
           </Link>
           <form method="GET" className="header__search-bar__form">
@@ -30,11 +37,12 @@ export const Header = () => {
               Registrarse
             </button>
             <button className="header__action-button-login btn">
-              Iniciar sesion
+              Iniciar sesión
             </button>
           </div>
         </section>
       </header>
+      <Menu/>
     </>
   );
 };
