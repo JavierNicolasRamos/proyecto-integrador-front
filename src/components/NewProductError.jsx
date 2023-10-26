bff74416347ee3f1ff5902c60198722ce187aa9d
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-10-25 22:25:06 -0300

    NewProduct page in progress

---
+++ src/components/NewProductError.jsx
@@ -0,0 +1,11 @@
+
+
+function NewProductError() {
+  return (
+    <div className="Error">
+      <p>Por favor chequea que la información sea correcta</p>
+    </div>
+  );
+}
+
+export default NewProductError;
