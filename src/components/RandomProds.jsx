8c8b04aa9fd4b211e1203f70b3719c83f8489a79
Federico Galan <fegv.2@hotmail.com.ar>
2023-10-27 17:42:22 -0300

    feat: Render components with real data fetched from the backend

--- src/components/RandomProds.jsx
+++ src/components/RandomProds.jsx
@@ -1,17 +1,47 @@
-import Card  from './Card'
-import productos  from './Product'
+import axios from "axios";
+import Card from "./Card";
+// import productos from "./Product";
+import { useEffect, useState } from "react";
 
 export const RandomProds = () => {
+
+  const [randomProds, setRandomProds] = useState([])
+  
+  useEffect(() => {
+    async function getRandomProds() {
+      try {
+        const response = await axios.get(`http://localhost:8002/instrumentos`, {
+          params: {
+            page: 1,
+            size: 10,
+            sort: 'nombre',
+          },
+        });
+        setRandomProds(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    getRandomProds();
+  }, []);
+
   return (
     <>
       <h2 className="home__title">Productos que pueden interesarte</h2>
       <div className="product-grid">
-      {
-        productos.map((producto) => (
-          <Card key={producto.id} producto={producto} />
-        ))
-      }
-    </div>
+        {randomProds.map((randomProduct) => (
+          // console.log(randomProduct)
+          <Card 
+            key={randomProduct.id} 
+            id={randomProduct.id} 
+            name={randomProduct.nombre} 
+            image={randomProduct.imagen[0].imagen}
+            score={randomProduct.puntuacion}
+            category={randomProduct.categoria.descripcion}   
+          />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
