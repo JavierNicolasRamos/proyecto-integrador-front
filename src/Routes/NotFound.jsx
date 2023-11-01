369a5a05cbf1f2c2d787fa24e4891f6e9f3f95b0
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-10-31 23:24:13 -0300

    NotFound route finished

--- src/Routes/NotFound.jsx
+++ src/Routes/NotFound.jsx
@@ -1,6 +1,22 @@
+import "../styles/NotFound.css";
+
 
 const NotFound = () => {
-  return <div>#404 NotFound</div>;
+  return (
+  <div className="NotFound">
+    
+    <img src="/src/images/404NotFound.svg" alt="404NotFound" />
+    
+    <h1>#404</h1>
+    <h1>Not Found</h1>
+
+
+
+    
+  </div>
+  
+  
+  )
 };
 
 export default NotFound;
