4cd512485ce460acacc1dd778df76006bd6f7538
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-01 12:28:44 -0300

    feat: Finish the Register form component

--- src/Routes/Register.jsx
+++ src/Routes/Register.jsx
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom"
+import { RegisterForm } from "../components/RegisterForm"
+import "../styles/Register.css"
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register-container">
+      <div className="register">
+        <div className="register__title">
+          <h3>Crear una cuenta</h3>
+          <Link to={"/login"}>
+            Ya tienes cuenta? Ingresa aquí.
+          </Link>
+        </div>
+        <RegisterForm/>
+      </div>
+    </div>
   )
 }
