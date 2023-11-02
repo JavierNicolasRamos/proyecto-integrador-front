4d534525c984db5f37dfd34c9a059c527a0bd6d4
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-02 15:26:58 -0300

    fix(bug): Month of the todayDate was wrong

--- src/helpers/generateActualDate.js
+++ src/helpers/generateActualDate.js
@@ -1,8 +1,9 @@
 export const generateActualDate = () => {
   //Creamos la fecha actual para comprar con la de nacimiento
   const todayDay = new Date().getDate()
-  const todayMonth = new Date().getMonth()
+  const todayMonth = new Date().getMonth() + 1
   const todayYear = new Date().getFullYear()
+
   const todayDate = `${todayYear}-${todayMonth}-${todayDay}`
 
   return todayDate
