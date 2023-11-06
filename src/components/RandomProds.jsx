ec18060654f653300d99b12362302f5a9d6c52bb
mariiasol <fmariasol93@gmail.com>
2023-11-05 22:06:23 -0600

    paginado en randomsprods-home

--- src/components/RandomProds.jsx
+++ src/components/RandomProds.jsx
@@ -6,28 +6,62 @@ import { useEffect, useState } from "react";
 export const RandomProds = () => {
 
   const [randomProds, setRandomProds] = useState([])
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   
-  useEffect(() => {
-    async function getRandomProds() {
-      try {
-        const response = await axios.get(`http://localhost:8001/instrumentos`, {
-          params: {
-            page: 1,
-            size: 10,
-            sort: 'nombre',
-          },
-        });
-        setRandomProds(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+  const params = {
+    page: currentPage - 1,
+    size: 10,
+    sort: "id,asc",
+  };
+  
+  
+// Obtain paged products
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8001/instrumentos/paginado`,
+      { params }
+    );
+    if (response.status === 200) {
+      setRandomProds(response.data.content);
+    } else {
+      console.error("La solicitud a la API no fue exitosa");
     }
-  
-    getRandomProds();
-  }, []);
+  } catch (error) {
+    console.error("Error al obtener datos de la API:", error);
+  }
+};
 
+useEffect(() => {
+  fetchProducts();
+}, [currentPage]);
+
+const handlePageChange = (page) => {
+  setCurrentPage(page);
+};
+
+// Obtain Products Quantuty
+const fetchTotalProducts = async () => {
+  try {
+    const response = await axios.get(`http://localhost:8001/instrumentos`);
+    if (response.status === 200) {
+      const total = response.data.length;
+      const totalPages = Math.floor(total / 10) + 1;
+      setTotalPages(totalPages);
+    } else {
+      console.error("La solicitud a la API no fue exitosa");
+    }
+  } catch (error) {
+    console.error("Error al obtener datos de la API:", error);
+  }
+};
+
+useEffect(() => {
+  fetchTotalProducts();
+}, []);
   return (
-    <>
+    <div>
       <h2 className="home__title">Productos que pueden interesarte</h2>
       <div className="product-grid">
         {randomProds.map((randomProduct) => (
@@ -42,6 +76,32 @@ export const RandomProds = () => {
           />
         ))}
       </div>
-    </>
+      <div className="pageNumbersDiv">
+          <button
+            className="pageNumber currentNumberPage"
+            onClick={() => setCurrentPage(currentPage === 1 ? currentPage : currentPage - 1)}
+          >
+            <img src="/src/images/PrevPage.svg" alt="PrevPage" />
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              className={`pageNumber ${
+                index + 1 === currentPage ? "currentNumberPage" : ""
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            className="pageNumber currentNumberPage"
+            onClick={() => setCurrentPage(currentPage + 1 > totalPages? currentPage : currentPage + 1 )}
+          >
+            <img src="/src/images/NextPage.svg" alt="PrevPage" />
+          </button>
+        </div>
+</div>
+
   );
 };
