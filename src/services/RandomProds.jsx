782645e3e1ba721433a543d2c46d90ccd0856463
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-06 17:04:00 -0300

    refactor service (RandomProds): Change on the service to display the results

--- src/services/RandomProds.jsx
+++ src/services/RandomProds.jsx
@@ -9,13 +9,15 @@ export const fetchRandomProds = async () => {
         404: "No se encontraron productos"
     };
   
-    let res;
+    let res
+
     try {
-        res = await axios.get(`http://localhost:8001/instrumentos`);
+        const {data} = await axios.get(`http://localhost:8001/instrumentos`);
+        res = data
     } catch (e) {
         if (errorMessages[e.status]) {
             throw new Error(errorMessages[e.status]);
         }
     }
-    return res.data;
-    };
+    return res;
+};
