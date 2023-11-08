933299c01208db75a3ae9ceecb70d1c9375915bf
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-11-08 19:36:26 -0300

    added category path in Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,8 +5,31 @@ export const Menu = () => {
   const createProductPath = location.pathname === "/product/create";
   const productListPath = location.pathname === "/product/list";
   const adminPanelPath = location.pathname === "/adminpanel";
+  const createCategoryPath = location.pathname === "/category/create";
+  const categoryListPath = location.pathname === "/category/list";
 
-  if (!createProductPath && !productListPath && !adminPanelPath) {
+  if (createProductPath || productListPath) {
+    return (
+      <nav className="navigation-menu">
+        <Link to={"/product/create"}>Crear producto</Link>
+        <Link to={"/product/list"}>Lista de productos</Link>
+      </nav>
+    );
+  } else if (createCategoryPath || categoryListPath) {
+    return (
+      <nav className="navigation-menu">
+        <Link to={"/category/create"}>Crear categoría</Link>
+        <Link to={"/category/list"}>Lista de categorías</Link>
+      </nav>
+    );
+  } else if (adminPanelPath) {
+    return (
+      <nav className="navigation-menu">
+        <Link to={"/home"}>Inicio</Link>
+        <Link to={"/home"}>Cuenta</Link> 
+      </nav>
+    );
+  } else {
     return (
       <nav className="navigation-menu">
         <Link to={"/home"}>Inicio</Link>
@@ -16,21 +39,5 @@ export const Menu = () => {
     );
   }
 
-  if (createProductPath || productListPath) {
-    return (
-      <nav className="navigation-menu">
-        <Link to={"/product/create"}>Crear Productos</Link>
-        <Link to={"/product/list"}>Lista de productos</Link>
-      </nav>
-    );
-  }
 
-  if (adminPanelPath) {
-    return (
-      <nav className="navigation-menu">
-        <Link to={"/home"}>Inicio</Link>
-        <Link to={"/home"}>Cuenta</Link> 
-      </nav>
-    );
-  }
 };
