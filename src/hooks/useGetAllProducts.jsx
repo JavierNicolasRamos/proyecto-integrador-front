8a5de54beae9a9ab403bce600169abb5ef640e20
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-10 14:36:48 -0300

    refactor: hook modified

--- src/hooks/useGetAllProducts.jsx
+++ src/hooks/useGetAllProducts.jsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { getAllProducts } from "../services/Products";
 
 export const useGetAllProducts = () => {
-    const [ products , setProducts ] = useState([]);
-  
+    const [ products , setProducts ] = useState();
+
     useEffect(() => {
         getAllProducts()
         .then( products => setProducts( products ) )
     }, []);
-  
-    return { products };
+
+    return products;
   }
