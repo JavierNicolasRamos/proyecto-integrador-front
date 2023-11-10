34416a14336f1ba0a37176002db1e2900c97ccff
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-10 10:51:13 -0300

    feat: display total of products

--- src/Routes/ProductPanel.jsx
+++ src/Routes/ProductPanel.jsx
@@ -1,14 +1,17 @@
 import { CategoryList } from "../components/CategoryList"
 import { PaginateButtons } from "../components/PaginateButtons"
 import { RandomProds } from "../components/RandomProds"
+import { useGetAllProducts } from "../hooks/useGetAllProducts"
 import "../styles/ProductPanel.css"
 
 export const ProductPanel = () => {
+
+  const { products } = useGetAllProducts()
   
   return (
     <>
       <div className="product__total">
-        <p>25 productos</p>
+        <p>{products.length} productos</p>
       </div>
       <div className="product__content">
         <CategoryList />
