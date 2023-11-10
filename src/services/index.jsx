1ffdaaeac2faf2f05fa2ce19f9f89626bce8e991
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-09 23:36:15 -0300

    errors fixed

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export { RandomProds } from "./RandomProds";
-export { Category } from "./Category"
-export { Products } from "./Products"
-=======
 export { fetchRandomProds } from "./RandomProds";
-export { fetchCategory, postCategory } from "./Category";
+export { getAllCategories, postCategory } from "./Category";
 export { fetchInstrument/*, postInstruments*/ } from "./Instrument";
->>>>>>> main
