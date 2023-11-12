51512a53d13547ac1f4b0e39d570954bfeea542a
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-11-12 15:57:01 -0300

    category get with images working

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,28 +1,19 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "../styles/CategoryCard.css";
 
-export const CategoryCard = ({name/*, image*/}) => {
+export const CategoryCard = ({ name, image }) => {
+  const imageUrl = image && image.image;
+
+  const backgroundImageStyle = {
+    backgroundImage: imageUrl ? `url(${imageUrl})` : "none",
+  };
+
   return (
-    <div className="category-card">
-
-        <h1>{name}</h1>
-
+    <div className="category-card" style={backgroundImageStyle}>
+      <h1>{name}</h1>
     </div>
-  )
-}
-
-//TODO: Decidir si esto se queda o se va
-
-// export const CategoryCard = ({name, image}) => {
-//   return (
-//     <div className="category-card">
-
-//         <h1>{name}</h1>
-//         <img src={image} alt={name}/>
-
-//     </div>
-//   )
-// }
+  );
+};
 
 CategoryCard.propTypes = {
   name: PropTypes.string.isRequired,
