4be2f0f663a7c1e47600e757558af07766798c31
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-26 21:58:04 -0300

    refactor: helpers modified

---
+++ src/helpers/validateRegisterForm.js
@@ -0,0 +1,37 @@
+import {
+  validateName,
+  validateEmail,
+  validatePassword,
+} from "./index";
+
+export const validateRegisterForm = (data) => {
+  const validations = {
+    name: [
+      validateName, 
+      "Por favor, ingresa solo letras y espacios en blanco para el nombre y apellido"
+    ],
+    surname: [
+      validateName,
+      "Por favor, ingresa solo letras y espacios en blanco para el nombre y apellido",
+    ],
+    password: [
+      validatePassword,
+      "La contraseña debe tener al menos 8 caracteres, incluyendo al menos una letra mayúscula, una letra minúscula y un número",
+    ],
+    email: [
+      validateEmail, 
+      "Ingresa una dirección de correo electrónico válida "
+    ],
+  };
+
+  const errors = {};
+
+  for (const field in validations) {
+    const [validate, message] = validations[field];
+    if (validate(data[field])) {
+      errors[field] = message;
+    }
+  }
+
+  return errors;
+};
