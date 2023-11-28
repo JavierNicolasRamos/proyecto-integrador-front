a58e0cbcf7b941fb645cb483a67434a9da4e792c
Ignacio Gibbs <ignacio.gibbs@gmail.com>
2023-11-28 15:46:19 -0300

    adding url to sharesocial component

--- src/components/Characteristics.jsx
+++ src/components/Characteristics.jsx
@@ -5,11 +5,14 @@ import { ShareSocial } from "./ShareSocial";
 
 export const Characteristics = ({characteristics}) => {
 
+  const id = window.location.pathname.split("/")[2]
+  const url = `http://localhost:3000/product/${id}` //TODO: cambiar por la url de producción
+  
   return (
     <div className="characteristics-container">
       <div className="characteristics-container__title">
-        <h2>Caracteristicas</h2>
-        <ShareSocial shareUrl="" name="" /*TODO: Agregar url y nombre */ /> 
+        <h2>Características</h2>
+        <ShareSocial shareUrl={url} name={characteristics.name} /> 
         {/* <img src="/src/images/share.svg" alt="Icono para compartir el producto" /> */}
       </div>
           {characteristics.map((characteristic, index) => (
