875c25eca42cf6e4d317477c7337af80fdeb68b4
Federico Galan <fegv.2@hotmail.com.ar>
2023-11-28 13:00:51 -0300

    Fixed MobileSearchBar

--- src/components/MobileSearchBar.jsx
+++ src/components/MobileSearchBar.jsx
@@ -1,13 +1,24 @@
 import "../styles/MobileSearchBar.css"
 
-export const MobileSearchBar = () => (
-  <form method="GET" className="header__mobile-search-bar__form">
-    <input
-      className="header__mobile-search-bar__form__input"
-      placeholder="¿Qué estás buscando?"
-      type="search"
-      name="mobileSearch" 
-      id="mobileSearch" 
-    />
-  </form>
-);
+export const MobileSearchBar = () => {
+
+  const isHome = location.pathname === "/home";
+
+  return (
+    <>
+      {
+        isHome 
+        ? <form method="GET" className="header__mobile-search-bar__form">
+          <input
+            className="header__mobile-search-bar__form__input"
+            placeholder="¿Qué estás buscando?"
+            type="search"
+            name="mobileSearch" 
+            id="mobileSearch" 
+          />
+        </form>
+        : ''
+      }
+    </>
+  )
+}
