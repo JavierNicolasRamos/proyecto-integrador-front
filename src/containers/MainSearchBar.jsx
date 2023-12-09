d8bba17d97a0c2aad93e5a72e426e76ce983970a
dmartin07 <dariomartinbenavidez@hotmail.com>
2023-12-09 17:54:43 -0300

    mainSearchBar minor fixes

--- src/containers/MainSearchBar.jsx
+++ src/containers/MainSearchBar.jsx
@@ -122,11 +122,14 @@ export const MainSearchBar = () => {
               image={image && image[0] && image[0].image}
             />
           ))}
-      {searchedInstruments && (
+      {Array.isArray(searchedInstruments) && searchedInstruments.length > 0 && (
         <RandomInstruments
           instruments={searchedInstruments}
           title="Resultados de tu búsqueda"
         />
+      )}
+      {Array.isArray(searchedInstruments) && searchedInstruments.length === 0 && (
+        <h3 className="mainSearcBar-NoResults">No hubo resultados para tu búsqueda</h3>
       )}
     </div>
   );
