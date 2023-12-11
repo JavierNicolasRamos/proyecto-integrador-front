5ad4895b7b28fec518cfb4f4ff66174ac9da4792
Federico Galan <fegv.2@hotmail.com.ar>
2023-12-10 22:54:29 -0300

    NotReserveOwn added

---
+++ src/hooks/usePostBooking.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { postBooking } from "../services/Booking"
+import { useState } from "react";
+
+export const usePostBooking = () => {
+
+  const navigate = useNavigate();
+  const [isFetching, setIsFetching] = useState(true)
+
+  const handlerConfirm = (formData) => {
+
+    console.log(formData)
+    
+    postBooking(formData)
+    .then(()=>{
+      navigate("/reservationSuccess/")
+    })
+    .catch((e) => {
+      navigate("/reserveDenied");
+    })
+    .finally(()=>{
+      setIsFetching(false)
+    })
+
+  }
+
+  return { handlerConfirm }
+}
